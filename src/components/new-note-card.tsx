e0115744ae0f2ed028755db121fea960f55a94f1
Jonas Souza <jonasmzsouza@gmail.com>
2024-02-07 22:12:33 -0300

    feat: create and save notes in state, save notes in storage

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -3,7 +3,11 @@ import { X } from "lucide-react";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { toast } from "sonner";
 
-export function NewNoteCard() {
+interface NewNoteCardProps {
+  onNoteCreated: (content: string) => void;
+}
+
+export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
   const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true);
   const [content, setContent] = useState("");
 
@@ -21,7 +25,9 @@ export function NewNoteCard() {
 
   function handleSaveNote(event: FormEvent) {
     event.preventDefault();
-    console.log(content);
+    onNoteCreated(content);
+    setContent("");
+    setShouldShowOnboarding(true);
     toast.success("Nota criada com sucesso!");
   }
 
@@ -70,6 +76,7 @@ export function NewNoteCard() {
                   autoFocus
                   className="text-sm leading-6 text-slate-400 bg-transparent resize-none outline-none"
                   onChange={handleContentChange}
+                  value={content}
                 ></textarea>
               )}
             </div>
